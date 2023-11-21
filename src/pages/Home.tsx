fdaaf133a2040600dee3127a6ba6932363f578ce
erickkv <erickkiehnle@gmail.com>
2023-11-20 20:33:53 -0600

    creado convertidor

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,36 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import React, { useState } from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonInput, IonButton } from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const [montoDolares, setMontoDolares] = useState<number>(0);
+  const [montoQuetzales, setMontoQuetzales] = useState<number>(0);
+
+  const convertirQuetzales = () => {
+    const tasaCambio = 8;
+    const resultado = montoQuetzales / tasaCambio;
+    setMontoDolares(resultado);
+  };
+
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Convertidor de Divisas</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+      <IonContent className="ion-padding">
+        <IonInput
+          type="number"
+          placeholder="Monto en Quetzales"
+          value={montoQuetzales}
+          onIonChange={(e) => setMontoQuetzales(parseFloat((e.detail.value!)))}
+        ></IonInput>
+        <IonButton expand="full" onClick={convertirQuetzales}>
+          Convertir
+        </IonButton>
+        <p>Resultado: {montoDolares} dólares </p>
       </IonContent>
     </IonPage>
   );
